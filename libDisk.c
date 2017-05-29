cf881d89560d7729248ee2df48d80e049984ec73
Bradley <brschmidt94@gmail.com>
2017-05-29 14:59:38 -0700

    asdsd

--- libDisk.c
+++ libDisk.c
@@ -5,29 +5,34 @@
 #include "libDisk.h"
 
 int openDisk(char *filename, int nBytes) {
-	FILE disk;
+	FILE *disk;
+	int diskNum;
 	
 	//is nBytes the # of BLOCKS? or the number of bytes of the overall file
 	//% 256 = 0
 	
-	if(access(filename, F_OK) != -1) { //If the file already exists
-		if(nBytes > 0) //Overwrite existing file
-			disk = fopen(filename, "w+");
-		else//nBytes == 0. Open existing file as read-only
-			disk = fopen(filename, "r");
+	if(nBytes % BLOCKSIZE != 0) {
+		if(access(filename, F_OK) != -1) { //If the file already exists
+			if(nBytes > 0) //Overwrite existing file
+				disk = fopen(filename, "w+");
+			else//nBytes == 0. Open existing file as read-only
+				disk = fopen(filename, "r");
+		}
+		else //We are making a new file
+			disk = fopen(filename, "w");
 	}
-	else //We are making a new file
-		disk = fopen(filename, "w");
-
+	else
+		diskNum = -1;
+		
 	return fileno(disk);
 }
 
 int readBlock(int disk, int bNum, void *block) {
 	int status = 0; //Stores error/success state
-	int fileOffset;
+	off_t *fileOffset;
 	
 	//Index into file by block number
-	if(fileOffset = lseek(disk, bNum * BLOCKSIZE, SEEK_CUR) >= 0)
+	if((fileOffset = lseek(disk, bNum * BLOCKSIZE, SEEK_CUR)) >= 0)
 		status = fread(block, sizeof(char), bNum * BLOCKSIZE, fileOffset);
 	
 	return status;
