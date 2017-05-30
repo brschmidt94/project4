0a527030bbf2a565c5d7c0c39ee380c9d906ff73
Bradley <brschmidt94@gmail.com>
2017-05-30 12:37:35 -0700

    new

--- libTinyFS.c
+++ libTinyFS.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 #include "tinyFS.h"
 #include "TinyFS_errno.h"
 
@@ -11,3 +14,33 @@ int tfs_writeFile(fileDescriptor FD);
 int tfs_deleteFile(fileDescriptor FD);
 int tfs_readByte(fileDescriptor FD);
 int tfs_seek(fileDescriptor FD, int offset);
+int verifyFormat(char *filename);
+
+int diskSize = 0;
+
+int tfs_mkfs(char *filename, int nBytes) {
+	diskSize = nBytes;
+	
+	
+	
+	return 1;
+}
+
+int tfs_mount(char *filename) {
+	int diskNum = -1;
+	//Where is nBytes set?
+	
+	if(verifyFormat(filename))
+		diskNum = openDisk(filename, nBytes);
+	else
+		diskNum = -4; //ERROR: IMPROPER DISK FORMAT
+	
+	return diskNum;
+}
+
+int verifyFormat(char *filename) {
+	
+	
+	return 1;
+}
+
